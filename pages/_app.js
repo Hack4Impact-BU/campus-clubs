99cc24d2883d349b73c79daa4e1f07282ff69286
Manuja DeSilva <mdesilva@bu.edu>
2020-09-28 22:39:11 -0400

    dont display header in login

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,38 @@ import "bulma/css/bulma.min.css";
 import "../styles/navigation.scss";
 
 import Navigation from '../components/Navigation';
+import App from "next/app";
+import React from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return (
-  <>
-  <Navigation/>
-  <div className="container">
-    <Component {...pageProps} />
-  </div>
-  </>
-  )
+class MyApp extends App {
+
+  static async getInitialProps(props) {
+    const {Component, ctx} = props;
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return {pageProps}
+  }
+
+  render() {
+    const {Component, pageProps} = this.props;
+    return (
+      <>
+      {
+       (Component.name == "LoginView") ? <Component {...pageProps} /> : 
+       <>
+         <Navigation/>
+         <div className="container">
+           <Component {...pageProps} />
+          </div>
+        </>
+      }
+      </>
+      )
+  }
 }
 
 export default MyApp
