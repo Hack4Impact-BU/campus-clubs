5f10b5cc1b6cf0c37c8a48e8d0baa78348737dd0
Manuja DeSilva <mdesilva@bu.edu>
2020-10-06 14:29:48 -0400

    pass required prop to Wrapper

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -3,7 +3,7 @@ import styles from "./dashboard.module.scss"
 
 export default function Dashboard() {
     return (
-        <Wrapper component={undefined}>
+        <Wrapper component="Dashboard">
             <h1> Dashboard </h1>
             <p> Nothing here yet</p>
         </Wrapper>
