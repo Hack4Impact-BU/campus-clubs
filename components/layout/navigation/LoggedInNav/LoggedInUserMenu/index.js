c9eea06b01dd46af17e5fd2256a5d664917882e8
Gagandeep Kang <gskang@bu.edu>
2020-10-29 17:33:29 -0400

    Revert "profile page"
    This reverts commit 3985f39181654c541977f17560992905452a5ebb.

--- components/layout/navigation/LoggedInNav/LoggedInUserMenu/index.js
+++ components/layout/navigation/LoggedInNav/LoggedInUserMenu/index.js
@@ -2,36 +2,34 @@ import Link from "next/link";
 import styles from "./LoggedInUserMenu.module.scss";
 
 export default class LoggedInUserMenu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dropdownActive: false,
-    };
-  }
 
-  handleMenuClick = () => {
-    this.setState((state) => ({ dropdownActive: !state.dropdownActive }));
-  };
+    constructor(props) {
+        super(props);
+        this.state = {
+            dropdownActive: false
+        }
+    }
 
-  render() {
-    return (
-      <div
-        className={`navbar-item has-dropdown ${
-          this.state.dropdownActive ? "is-active" : null
-        }`}
-        onClick={this.handleMenuClick}
-      >
-        <div className={styles.menu}>
-          <img src="avatar.png" />
-          <i className="fas fa-bars"></i>
-        </div>
+    handleMenuClick = () => {
+        this.setState(state => ({dropdownActive: !state.dropdownActive}))
+    }
+    
+    render() {
+        return (
+            <div className={`navbar-item has-dropdown ${this.state.dropdownActive ? "is-active" : null}`}  onClick={this.handleMenuClick}>
+                <div className={styles.menu}>
+                    <img src="avatar.png"/>
+                    <i className="fas fa-bars"></i>
+                </div>
 
-        <div className="navbar-dropdown" style={{ borderTop: "none" }}>
-          <Link href="/api/auth/signout">
-            <a className="navbar-item">Logout</a>
-          </Link>
-        </div>
-      </div>
-    );
-  }
+                <div className="navbar-dropdown" style={{borderTop: "none"}}>
+                    <Link href="/api/auth/signout">
+                        <a className="navbar-item">
+                            Logout
+                        </a>
+                    </Link>
+                </div>
+            </div>
+        )
+    }
 }
